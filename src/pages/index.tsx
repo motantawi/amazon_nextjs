02f5faa298f179694716694c89b32ce998749761
Mustafa Tantawy <106565969+motantawi@users.noreply.github.com>
2023-08-05 15:51:35 +0300

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import Banner from "@/components/Banner";
+import Products from "@/components/Products";
+import { ProductProps } from "../../type";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setAllProducts } from "@/store/nextSlice";
+import Head from "next/head";
+
+interface Props {
+  productData: ProductProps;
+}
+
+export default function Home({ productData }: Props) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setAllProducts({ allProducts: productData }));
+  }, [productData]);
+  return (
+    <>
+      <Head>
+        <title>Amazon | NextJS</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main>
+        <div className="max-w-screen-2xl mx-auto">
+          <Banner />
+          <div className="relative md:-mt020 lgl:-mt-32 xl:-mt-60 z-20 mb-10">
+            <Products productData={productData} />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+// SSR for data fetching
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return { props: { productData } };
+};
